ab8753108fe3c206895bec32b2509ea8a7d4ec11
Josh Ziegler <josh.ziegler@infinitetactics.com>
2023-05-02 16:05:15 +0000

    Factor out GetAllUsers and GetAllOpenTickets

--- main.go
+++ main.go
@@ -5,12 +5,76 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/xanzy/go-gitlab"
 )
 
+// GetEnv variable or use the default value if it is not set (i.e. empty).
+func GetEnv(varName, defaultValue string) string {
+	val := strings.TrimSpace(os.Getenv(varName))
+	if val == "" {
+		val = defaultValue
+	}
+	return val
+}
+
+// GetEnv variable or use the default value if it is not set (i.e. empty).
+func GetEnvInt(varName string, defaultValue int) int {
+	str := strings.TrimSpace(os.Getenv(varName))
+	if str == "" {
+		return defaultValue
+	}
+	val, err := strconv.Atoi(str)
+	if err != nil {
+		log.Fatalf("cannot convert environment variable to an integer: %s", err.Error())
+	}
+	return val
+}
+
+// GetAllUsers from the Gitlab instance.
+func GetAllUsers(git *gitlab.Client) ([]*gitlab.User, error) {
+	users, resp, err := git.Users.ListUsers(&gitlab.ListUsersOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving users from Gitlab: %s", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected response from Gitlab: %d %v", resp.StatusCode, resp.Response)
+	}
+	return users, nil
+}
+
+// GetAllOpenIssues from the Gitlab instance for the specific project.
+func GetAllOpenIssues(git *gitlab.Client, projectID int) (issues []*gitlab.Issue, err error) {
+	opt := &gitlab.ListProjectIssuesOptions{
+		ListOptions: gitlab.ListOptions{
+			PerPage: 100, // Gitlab's Max per page is 100
+			Page:    1,
+		},
+		State: gitlab.String("opened"),
+	}
+	for { // Get each page of results and add to the full set of issues
+		issueList, resp, err := git.Issues.ListProjectIssues(projectID, opt)
+		if err != nil {
+			return issues, fmt.Errorf("error retrieving issues from Gitlab: %s", err)
+		}
+		if resp.StatusCode != http.StatusOK {
+			return issues, fmt.Errorf("unexpected response from Gitlab: %d %v", resp.StatusCode, resp.Response)
+		}
+		issues = append(issues, issueList...)
+
+		// Get next page or exit if this was the last
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	return
+}
+
 func main() {
-	projectID := 111 // TODO: Allow to set at the CLI?
+	projectID := GetEnvInt("GITLAB_PROJ_ID", 111)
 	key := os.Getenv("GITLAB_API_KEY")
 	if key == "" {
 		log.Fatal("GITLAB_API_KEY environment variable is required")
@@ -19,69 +83,27 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error creating client: %v", err)
 	}
-	users, resp, err := git.Users.ListUsers(&gitlab.ListUsersOptions{})
+
+	// List All Users //////////////////////////////////////////////////////////
+	users, err := GetAllUsers(git)
 	if err != nil {
-		log.Fatalf("Error: %s\n", err.Error())
-	}
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Unesexped response from Gitlab: %d %v", resp.StatusCode, resp.Response)
-	}
-	fmt.Printf("Users (%d):\n", len(users))
-	for _, u := range users {
-		fmt.Printf("    - %s\n", u.Name)
-	}
-
-	///////////////////////////////////////////////////////////////////////////
-
-	// List issues I have created and are still open
-	// opt := &gitlab.ListIssuesOptions{
-	// 	ListOptions: gitlab.ListOptions{
-	// 		PerPage: 100, // Gitlab's Max per page is 100
-	// 		Page:    1,
-	// 	},
-	// 	State: gitlab.String("opened"),
-	// }
-	// issues, resp, err := git.Issues.ListIssues(opt)
-	// if err != nil {
-	// 	log.Fatalf("Error: %s\n", err.Error())
-	// }
-	// if resp.StatusCode != http.StatusOK {
-	// 	log.Fatalf("Unesexped response from Gitlab: %d %v", resp.StatusCode, resp.Response)
-	// }
-	// fmt.Println("Issues:")
-	// for _, i := range issues {
-	// 	fmt.Printf("  - %d %s\n", i.ID, i.Title)
-	// }
-
-	///////////////////////////////////////////////////////////////////////////
-	// List issues in AnalyticsGateway repo that are still open
-	opt := &gitlab.ListProjectIssuesOptions{
-		ListOptions: gitlab.ListOptions{
-			PerPage: 100, // Gitlab's Max per page is 100
-			Page:    1,
-		},
-		State: gitlab.String("opened"),
-	}
-	var allIssues []*gitlab.Issue
-	for {
-		issues, resp, err := git.Issues.ListProjectIssues(projectID, opt)
-		if err != nil {
-			log.Fatalf("Error: %s\n", err.Error())
+		log.Printf("Users: %s\n", err)
+	} else {
+		fmt.Printf("Users (%d):\n", len(users))
+		for _, u := range users {
+			fmt.Printf("    - %s\n", u.Name)
 		}
-		if resp.StatusCode != http.StatusOK {
-			log.Fatalf("Unesexped response from Gitlab: %d %v", resp.StatusCode, resp.Response)
-		}
-		allIssues = append(allIssues, issues...)
-
-		// Get next page or exit if this was the last
-		if resp.NextPage == 0 {
-			break
-		}
-		opt.Page = resp.NextPage
 	}
 
-	fmt.Printf("Issues (%d):\n", len(allIssues))
-	for _, i := range allIssues {
-		fmt.Printf("    - %d %s\n", i.IID, i.Title) // IID is the Project-specific ID
+	// List all open issues in the project /////////////////////////////////////
+	issues, err := GetAllOpenIssues(git, projectID)
+	if err != nil {
+		log.Printf("Issues: %s\n", err)
+	} else {
+		fmt.Printf("Issues (%d):\n", len(issues))
+		for _, i := range issues {
+			fmt.Printf("    - %d %s\n", i.IID, i.Title) // IID is the Project-specific ID
+			fmt.Printf("        - %+v\n", i.Labels)
+		}
 	}
 }
